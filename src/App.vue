9605e6fb003ea3ae2411034253916662e10eb0fc
lui5gl <lui5gl@outlook.com>
2025-01-31 14:12:29 -0600

    initialize Vue 3 project with Vite and TypeScript setup

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <section>
+      <h1>Todo List ✅</h1>
+      <form class="todo-form">
+        <input type="text" class="todo-input" placeholder="Add a new todo" />
+        <button class="add-input" type="submit">Add</button>
+      </form>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  padding: 25px;
+  min-height: 100dvh;
+  justify-content: center;
+}
+
+h1 {
+  font-size: 2rem;
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.todo-form {
+  display: flex;
+}
+
+.todo-input {
+  width: 100%;
+  border: none;
+  padding: 5px;
+  max-width: 250px;
+  border-radius: 2px;
+  margin-right: 10px;
+  border: 1px solid #262626;
+}
+
+.add-input {
+  border: none;
+  color: #fafafa;
+  padding: 5px 25px;
+  border-radius: 2px;
+  font-weight: 700;
+  background: #262626;
+}
+</style>
+
